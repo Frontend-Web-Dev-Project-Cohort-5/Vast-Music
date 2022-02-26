eafa207f0bf7605e282b3237415f2308fe31ff7c
deepizel <rocmancave@gmail.com>
2022-02-27 00:03:39 +0100

    added dev team section with profile images and description

--- src/Pages/About Us.js
+++ src/Pages/About Us.js
@@ -3,7 +3,56 @@ import Abouthdr from '../Conponents/Abouthdr'
 
 const AboutUs = () => {
   return (
+    <>
     <Abouthdr /> 
+    <br />
+    <br />
+    <h3>Development Team</h3>
+    <div>
+        <div className="row">
+          <div className="col-4">
+            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div></div>
+    );
+);
+
+
+    </>
   )
 }
 
