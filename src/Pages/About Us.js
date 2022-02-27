7403395191ebc32b58151ecd391c06c3a7af2770
deepizel <rocmancave@gmail.com>
2022-02-27 01:09:30 +0100

    added Dev team cards

--- src/Pages/About Us.js
+++ src/Pages/About Us.js
@@ -9,50 +9,65 @@ const AboutUs = () => {
     <br />
     <h3>Development Team</h3>
     {/* Dev team section */}
-    <div>
-        <div className="row">
-          <div className="col-4">
-            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-          <div className="col-4">
-            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-          <div className="col-4">
-            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-4">
-            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-          <div className="col-4">
-            <img src="./Hero.jpg" className="card-img-top" alt="..." width="200px; 200px;" />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div></div>
-    );
-);
-
-
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="Devteam3.jpg" class="card-img-top" alt="developer image"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in to
+          additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="Devteam2.jpg" class="card-img-top" alt="Developer image"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="Devteam4.jpg" class="card-img-top" alt="Developer img"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in to
+          additional content. This card has even longer content than the first to show
+          that equal height action.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="Devteam1.jpg" class="card-img-top" alt="Developer img"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in to
+          additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="" class="card-img-top" alt="Developer img"/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+</div>
     </>
   )
 }
