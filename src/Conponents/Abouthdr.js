66c0d357c53882715fdb6b2e24206ccab1497041
MbonuJennifer <MbonuifeomaJennifer@gmail.com>
2022-02-28 22:10:43 +0100

    Updated About us page.

--- src/Conponents/Abouthdr.js
+++ src/Conponents/Abouthdr.js
@@ -2,17 +2,25 @@ import React from 'react'
 
 const Abouthdr = () => {
   return (
-      <div>
-        <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
-     </div>
+    <div>
+      <div
+        id="carouselExampleDark"
+        className="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      ></div>
       <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="10000">
-            <img src="Hero.jpg" className="d-block w-100" alt="roku width:1512px; height:1300px;"/>
-            <div className="carousel-caption d-none d-md-block">
-              <h3>About Us</h3>
-              <p>The Vast music app was developed to help movie soundtrack lovers find their favorite movie soundtracks using just the movie title. We know movie titles stick with you for longer,so we built this app to help source that amazing tune you just cant forget and remember at the same time</p>
-              <p>Cheers!</p>
-            </div>
+        <div
+          className="carousel-item active HeroImage"
+          data-bs-interval="10000"
+        >
+          <img
+            src="Aboutus.png"
+            className="d-block w-100"
+            alt="roku width:1512px; height:1300px;"
+          />
+          <div className="carousel-caption carousel-text d-none d-md-block">
+            <h3>About Us</h3>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/About Us.js
+++ src/Pages/About Us.js
@@ -5,11 +5,22 @@ const AboutUs = () => {
   return (
     <>
       <Abouthdr />
-      <br />
-      <br />
+      <p className="paraIntro mt-5 mx-5 px-5 d-flex justify-content-center align-items-center">
+        The Vast music app was developed to help movie soundtrack lovers find
+        their favorite movie soundtracks using just the movie title. We know
+        movie titles stick with you for longer,so we built this app to help
+        source that amazing tune you just cant forget and remember at the same
+        time.
+      </p>
+      <p className="Cheer mb-5 mx-5 px-5 d-flex justify-content-center align-items-center">
+        Cheers!
+      </p>
+      <p className="Teamtext fs-3 fw-bold mx-5 d-flex justify-content-center align-items-center">
+        Development Team
+      </p>
       <h3>Development Team</h3>
       {/* Dev team section */}
-      <div className="row row-cols-1 row-cols-md-3 g-4">
+      <div className="row row-cols-1 row-cols-md-3 g-4 mx-5 px-5 style={{ width: '18rem' }}">
         <div className="col">
           <div className="card h-100">
             <img src="Devteam3.jpg" className="card-img-top" alt="developer" />
@@ -52,7 +63,7 @@ const AboutUs = () => {
           </div>
         </div>
       </div>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
+      <div className="row row-cols-1 row-cols-md-3 g-4 m-5 px-5 style={{ width: '18rem' }}">
         <div className="col">
           <div className="card h-100">
             <img src="Devteam1.jpg" className="card-img-top" alt="developer" />
