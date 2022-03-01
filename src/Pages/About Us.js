0773337bd23a9090f4c2581a99a2ee5cbe37a0fd
deepizel <rocmancave@gmail.com>
2022-03-01 11:48:35 +0100

    added new files

--- src/Pages/About Us.js
+++ src/Pages/About Us.js
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <Abouthdr /> 
     <br />
     <br />
-    <h3>Development Team</h3>
+    <h2>Development Team </h2>
     {/* Dev team section */}
     <div className="row row-cols-1 row-cols-md-3 g-4">
   <div className="col">
